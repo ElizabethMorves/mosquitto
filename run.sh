c36e7214fe3ce3cc7b740cc1c9042ffd8b409dc9
Elizabeth Morves <Elizabeth.Morves@gmail.com>
2019-01-24 04:01:39 +0300

    eclipse-mosquitto, auth-plug docker duild

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+chown mosquitto:mosquitto -R /var/lib/mosquitto
+
+if [ "$1" = 'mosquitto' ]; then
+    exec /usr/local/sbin/mosquitto -c /etc/mosquitto/mosquitto.conf
+fi
+
+exec "$@"
