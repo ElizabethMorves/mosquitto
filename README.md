7a7c4e1cbbe3028ace09a23dcff4f78e50b1859a
ElizabethMorves <Elizabeth.Morves@mail.ru>
2019-01-27 14:08:37 +0300

    f

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 docker build --no-cache -t uzh0r/mosquitto .
+
+
+docker run -dit --restart=always -d --name=mqtt --hostname=mqtt \
+-p 1883:1883 \
+-p 8883:8883 \
+-p 8083:8083 \
+-v /etc/Lets_Encrypt/domain/:/etc/Lets_Encrypt/domain:ro \
+-v /mosquitto/mosquitto.conf:/etc/mosquitto/mosquitto.conf:ro \
+-v /mosquitto/auth-plugin.conf:/etc/mosquitto.d/auth-plugin.conf:ro \
+uzh0r/mosquitto
--- mysql_structure.sql
+++ mysql_structure.sql
@@ -1,10 +1,6 @@
---
 -- Table structure for table `mqtt_users`
---
 
 DROP TABLE IF EXISTS `mqtt_users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mqtt_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL,
@@ -13,19 +9,10 @@ CREATE TABLE `mqtt_users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `mqtt_users_username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `mqtt_users`
---
-
---
 -- Table structure for table `mqtt_acls`
---
 
 DROP TABLE IF EXISTS `mqtt_acls`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mqtt_acls` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(25) NOT NULL,
@@ -34,9 +21,4 @@ CREATE TABLE `mqtt_acls` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `mqtt_acls_user_topic` (`username`,`topic`(228))
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `mqtt_acls`
---
 
